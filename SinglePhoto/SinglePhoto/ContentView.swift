e63fa5c6c765dea6626f271dd4b422679a7e7fb8
rodrigomota01 <roguitymsn@gmail.com>
2022-04-21 16:24:32 -0300

    iniciando projeto em swift

---
+++ SinglePhoto/SinglePhoto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SinglePhoto
+//
+//  Created by Rodrigo on 21/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SinglePhoto/SinglePhoto/SinglePhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SinglePhotoApp.swift
+//  SinglePhoto
+//
+//  Created by Rodrigo on 21/04/22.
+//
+
+import SwiftUI
+
+@main
+struct SinglePhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
